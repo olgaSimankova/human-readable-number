98c5980c18d49bf481641851a5f24fef191e4f0f
olgaSimankova <o.kotlyarova@gmail.com>
2022-01-05 13:19:11 +0300

    feat: add toReadable function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    const a = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const b = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+    if (number === 0) return 'zero'
+    let result = '', tempNum = number
+
+    if (tempNum >= 100) {
+        result += a[parseInt(number.toString()[0], 10)] + ' hundred'
+        tempNum %= 100;
+    }
+
+    //it's a single word
+    if (tempNum < 20) {
+        result += ' ' + a[tempNum]
+
+        //it's possibly two words
+    } else {
+        result += ' ' + b[parseInt(tempNum.toString()[0], 10)]
+        tempNum %= 10
+        if (tempNum) {
+            result += ' ' + a[tempNum]
+        }
+    }
+    return result.trim()
 }
